6ad1fa066ca4a11b05eb3e72277ca7a518e51a27
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-09-24 08:47:32 +0200

    Big rework of how the compiler operates; broken right now
    I'm not compiling something right… but I build this in a Straßenbahn
    in Berlin!

--- interp_threaded_opt.rkt
+++ interp_threaded_opt.rkt
@@ -1,131 +1,94 @@
 #lang racket
 
 (define (parse-file filename)
-  (list->vector
-   (filter (λ (c) (member c '(#\> #\< #\+ #\- #\. #\, #\[ #\])))
-           (string->list (file->string filename)))))
+  (filter (λ (c) (member c '(#\> #\< #\+ #\- #\. #\, #\[ #\])))
+          (string->list (file->string filename))))
 
-(struct jmp (amount) #:transparent)
-(struct jmp-forward jmp () #:transparent)
-(struct jmp-backward jmp () #:transparent)
+(struct loop (body) #:transparent)
 (struct add (amount) #:transparent)
 (struct shift (amount) #:transparent)
+(struct bf-write () #:transparent)
+(struct bf-read () #:transparent)
 
-(define (preprocess-loops! prog)
-  (for ([i (in-range (vector-length prog))])
-    (match (vector-ref prog i)
-      [#\[ (vector-set! prog i (find-matching prog i 1 'close))]
-      [#\] (vector-set! prog i (find-matching prog i -1 'open))]
-      [_ 42])))
+(define (parse-prog char-list)
+  (match char-list
+    [(cons #\+ rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (add 1) next) unparsed))]
+    [(cons #\- rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (add -1) next) unparsed))]
+    [(cons #\< rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (shift -1) next) unparsed))]
+    [(cons #\> rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (shift 1) next) unparsed))]
+    [(cons #\. rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (bf-write) next) unparsed))]
+    [(cons #\, rst)
+     (let-values ([(next unparsed) (parse-prog rst)])
+       (values (cons (bf-read) next) unparsed))]
+    [(cons #\[ rst)
+     (let*-values ([(body after-loop) (parse-prog rst)]
+                   [(next unparsed) (parse-prog after-loop)])
+       (values (cons (loop body) next) unparsed))]
+    [(cons #\] rst)
+     (values '() rst)]
+    ['() (values '() '())]))
 
 (define (combine-instrs prog)
-  (let ([new-prog '()])
-    (for ([i prog])
-      (cond
-        [(null? new-prog)
-         (set! new-prog (cons i '()))]
-        [(and (add? (car new-prog)) (eqv? i #\+))
-         (set! new-prog (cons (add (+ (add-amount (car new-prog)) 1)) (cdr new-prog)))]
-        [(and (add? (car new-prog)) (eqv? i #\-))
-         (set! new-prog (cons (add (- (add-amount (car new-prog)) 1)) (cdr new-prog)))]
-        [(and (shift? (car new-prog)) (eqv? i #\>))
-         (set! new-prog (cons (shift (+ (shift-amount (car new-prog)) 1)) (cdr new-prog)))]
-        [(and (shift? (car new-prog)) (eqv? i #\<))
-         (set! new-prog (cons (shift (- (shift-amount (car new-prog)) 1)) (cdr new-prog)))]
-        [(eqv? i #\+)
-         (set! new-prog (cons (add 1) new-prog))]
-        [(eqv? i #\-)
-         (set! new-prog (cons (add -1) new-prog))]
-        [(eqv? i #\>)
-         (set! new-prog (cons (shift 1) new-prog))]
-        [(eqv? i #\<)
-         (set! new-prog (cons (shift -1) new-prog))]
-        [else (set! new-prog (cons i new-prog))]))
-    (list->vector (reverse new-prog))))
+  (let ([new-prog (list (car prog))])
+    (for ([i (cdr prog)])
+      (match (cons i (car new-prog))
+        [(cons (add a1) (add a2))
+         (set! new-prog (cons (add (+ a1 a2)) (cdr new-prog)))]
+        [(cons (shift s1) (shift s2))
+         (set! new-prog (cons (shift (+ s1 s2)) (cdr new-prog)))]
+        [(cons (loop body) _)
+         (set! new-prog (cons (loop (combine-instrs body)) new-prog))]
+        [_ (set! new-prog (cons i new-prog))]))
+    (reverse new-prog)))
 
-(define (find-matching prog start offset kind [stack 0])
-  (define (close? x) (or (jmp-backward? x) (eqv? x #\])))
-  (define (open? x) (or (jmp-forward? x) (eqv? x #\[)))
-  (define addr (+ start offset))
-  (let-values ([(needle-pred other-pred bump jmp-maker)
-                (if (eq? kind 'close)
-                    (values close? open? 1 jmp-forward)
-                    (values open? close? -1 jmp-backward))])
-    (if (needle-pred (vector-ref prog addr))
-        (if (zero? stack)
-            (jmp-maker offset)
-            (find-matching prog start (+ bump offset) kind (- stack 1)))
-        (if (other-pred (vector-ref prog addr))
-            (find-matching prog start (+ bump offset) kind (+ stack 1))
-            (find-matching prog start (+ bump offset) kind stack)))))
-
-(define (compile program c-ip jmp-targets inst-cache)
-  (hash-ref
-   inst-cache c-ip
-   (λ ()
-     (let ([compiled
-            (if (< c-ip (vector-length program))
-                (match (vector-ref program c-ip)
-                  [(add amount) (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                                  (λ (state sp)
-                                    (println "add")
-                                    (vector-set! state sp (+ (vector-ref state sp) amount))
-                                    (rest-progn state sp)))]
-                  [(shift amount) (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                                    (λ (state sp)
-                                      (println "shift")
-                                      (rest-progn state (+ sp amount))))]
-                  [#\+ (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                         (λ (state sp)
-                           (println "+")
-                           (vector-set! state sp (+ (vector-ref state sp) 1))
-                           (rest-progn state sp)))]
-                  [#\- (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                         (λ (state sp)
-                           (println "-")
-                           (vector-set! state sp (- (vector-ref state sp) 1))
-                           (rest-progn state sp)))]
-                  [#\> (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                         (λ (state sp)
-                           (println ">")
-                           (rest-progn state (+ sp 1))))]
-                  [#\< (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                         (λ (state sp)
-                           (println "<")
-                           (rest-progn state (- sp 1))))]
-                  [#\. (let ([rest-progn (compile program (+ 1 c-ip) jmp-targets inst-cache)])
-                         (λ (state sp)
-                           (println ".")
-                           (display (integer->char (vector-ref state sp)))
-                           (rest-progn state sp)))]
-                  [(jmp-forward target)
-                   (letrec ([loop-start (λ (state sp)
-                                          (println "[")
-                                          (if (zero? (vector-ref state sp))
-                                              (loop-end state sp)
-                                              (loop-body state sp)))]
-                            [loop-past-end (compile program (+ c-ip target 1) jmp-targets inst-cache)]
-                            [loop-end (compile program (+ c-ip target) (cons loop-start loop-past-end) inst-cache)]
-                            [loop-body (compile program (+ 1 c-ip) null inst-cache)])
-                     loop-start)]
-                  [(jmp-backward _)
-                   (λ (state sp)
-                     (println "]")
-                     (if (zero? (vector-ref state sp))
-                         ((cdr jmp-targets) state sp)
-                         ((car jmp-targets) state sp)))])
-                (λ (_state _sp)           ; finished compiling program
-                  void))])
-       (hash-set! inst-cache c-ip compiled)
-       compiled))))
+(define/match (compile program)
+  [('()) (λ (sp st) 'finished)]
+  [((cons instr instr-rst))
+   (let ([rest-progn (compile instr-rst)])
+     (match instr
+       [(add amount)
+        (λ (sp st)
+          (printf "+~a" amount)
+          (vector-set! st sp (+ amount (vector-ref st sp)))
+          (rest-progn sp st))]
+       [(shift amount)
+        (λ (sp st)
+          (printf ">~a" amount)
+          (rest-progn (+ sp amount) st))]
+       [(bf-write)
+        (λ (sp st)
+          (display (integer->char (vector-ref st sp)))
+          (rest-progn sp st))]
+       [(bf-read)
+        (λ (sp st)
+          (error "unimplemented")
+          (rest-progn sp st))]
+       [(loop body)
+        (let ([body-progn (compile body)])
+          (letrec ([the-loop (λ (sp st)
+                               ;; Help! Can I make this more efficient?
+                               (body-progn sp st)
+                               (if (zero? (vector-ref st sp))
+                                   (rest-progn sp st)
+                                   (the-loop sp st)))])
+            the-loop))]))])
 
 (define (run-file filename)
-  (let ([program (parse-file filename)])
-    (preprocess-loops! program)
-    ((compile (combine-instrs program) 0 null (make-hash)) (make-vector 10000) 5000)))
+  (displayln "Parsing...")
+  (let-values ([(program _) (parse-prog (parse-file filename))])
+    (displayln "Parsed. Executing...")
+    ((compile (combine-instrs program)) 5000 (make-vector 10000))))
 
 #;
-(let* ([the-file (command-line #:program "interp_threaded_opt" #:args (filename) filename)]
-       [program (parse-file the-file)])
-  (preprocess-loops! program)
-  ((compile (combine-instrs program) 0 null (make-hash)) (make-vector 10000) 5000))
+(let* ([the-file (command-line #:program "interp_threaded_opt" #:args (filename) filename)])
+  (run-file the-file))
