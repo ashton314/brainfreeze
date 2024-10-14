06b9ff4ceef85df7829e97dffee422580759d555
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-10-14 16:04:53 -0600

    mult-block-0 instruction implemented; mandel working again

--- native.rkt
+++ native.rkt
@@ -39,7 +39,7 @@ _ptr:
 
 (define (compile-to-asm program)
   (displayln prelude)
-  (emit-c program)
+  (emit-asm program)
   (displayln postlude))
 
 ;; Register reservations:
@@ -48,6 +48,10 @@ _ptr:
 ;; x20 - tape start
 ;; x21 - data pointer
 ;; x22 - misc
+;; x23 - misc
+;; x24 - misc
+;; x25 - misc
+;; x26 - mult linker
 ;; x11 - misc
 
 (define fresh-label
@@ -57,7 +61,41 @@ _ptr:
           (format "LBB0_~a" counter)
         (set! counter (+ 1 counter))))))
 
-(define/match (emit-c program)
+(define (i/load src d1 d2 [comment ""])
+  (format "\tldrsb\t~a, [~a, ~a]~a\n" src d1 d2 comment))
+(define (i/load1 src d1 [comment ""])
+  (format "\tldrsb\t~a, [~a]~a\n" src d1 comment))
+(define (i/load-ptr src [comment ""]) (i/load src 'x20 'x21 comment))
+
+(define (i/store src d1 d2 [comment ""])
+  (format "\tstrb\t~a, [~a, ~a]~a\n" src d1 d2 comment))
+(define (i/store1 src d1 [comment ""])
+  (format "\tstrb\t~a, [~a]~a\n" src d1 comment))
+(define (i/store-ptr src [comment ""]) (i/store src 'x20 'x21 comment))
+
+(define (i/mov dest src [comment ""])
+  (format "\tmov\t~a, ~a~a\n" dest src comment))
+(define (i/movi dest val [comment ""])
+  (format "\tmov\t~a, #~a~a\n" dest val comment))
+
+(define (i/add dest arg1 arg2 [comment ""])
+  (format "\tadd\t~a, ~a, ~a~a\n" dest arg1 arg2 comment))
+(define (i/subs dest arg1 arg2 [comment ""])
+  (format "\tsubs\t~a, ~a, ~a~a\n" dest arg1 arg2 comment))
+(define (i/mul dest arg1 arg2 [comment ""])
+  (format "\tmul\t~a, ~a, ~a~a\n" dest arg1 arg2 comment))
+
+(define (mult-immediate dest arg1 arg2)
+  (cond
+    [(zero? arg2) (i/mov dest 'wzr)]
+    [(and (equal? dest arg1) (= 1 arg2)) ""]
+    [(= 1 arg2) (i/mov dest arg1)]
+    [else (string-append (i/mov 'x26 (i/i arg2)) (i/mul dest arg1 'x26))]))
+
+(define (i/i thing)
+  (format "#~a" thing))
+
+(define/match (emit-asm program)
   [('()) (displayln "")]
   [((cons instr instr-rst))
    (match instr
@@ -66,33 +104,51 @@ _ptr:
       (printf "\tldrsb\tw11, [x20, x21]\t;add ~a\n" amount) ; copy tape[ptr] to w11
       (printf "\tadd\tw11, w11, #~a\n" amount) ; increment w11
       (printf "\tstrb\tw11, [x20, x21]\n")       ; writeback w11 to tape[ptr]
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
      [(add-cell-0 dest)
       (printf "\tldrsb\tw11, [x20, x21]\t;add-cell-0\n") ; copy tape[ptr] to w11
-      (printf "\tmov\tw22, #0\n")
-      (printf "\tstrb\tw22, [x20, x21]\n") ; zero out current cell
+      (printf "\tstrb\twzr, [x20, x21]\n") ; zero out current cell
       (printf "\tadd\tx22, x21, #~a\n" dest) ; compute dest
       (printf "\tldrsb\tw23, [x20, x22]\n")
       (printf "\tadd\tw11, w11, w23\n")
       (printf "\tstrb\tw11, [x20, x22]\n") ; write sum to destination
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
+     [(mult-block-0 body)
+      (display (i/add 'x22 'x20 'x21))        ; put base offset in x22
+      (display (i/load1 'x23 'x22))           ; current value in x23
+      (for ([idx (sort (hash-keys body) <)]
+            #:unless (or (= idx 0) (zero? (hash-ref body idx))))
+        (display (i/add 'x24 'x22 (i/i idx))) ; compute offset address → x24
+        (display (i/load1 'x25 'x24))         ; put that value in x25
+        (display                              ; x11 ← x23 * analyzed
+         (mult-immediate 'x11 'x23
+                         (abs (hash-ref body idx))))
+        (cond
+          [(negative? (hash-ref body idx))
+           (display (i/subs 'w11 'w25 'w11))  ; x11 = x25 - x11 (value of this cell)
+           ]
+          [else
+           (display (i/add 'w11 'w25 'w11))   ; x11 = x25 + x11 (value of this cell)
+           ])
+        (display (i/store1 'w11 'x24)))       ; write that back
+      (display (i/store-ptr 'wzr))            ; zero out current cell
+      (emit-asm instr-rst)]
      [(set-cell value)
-      ;; (printf "  tape[ptr] = ~a;\n" value)
       (printf "\tmov\tw11, #~a\t;set ~a\n" value value)
       (printf "\tstrb\tw11, [x20, x21]\n")
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
      [(shift amount)
       ;; (printf "  ptr += ~a;\n" amount)
       (printf "\tadd\tx21, x21, #~a\t;shift ~a\n" amount amount)
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
      [(bf-write)
       (printf "\tldrb\tw0, [x20, x21]\t;write\n")
       (printf "\tbl _putchar\n")
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
      [(bf-read)
       (printf "\tbl _getchar\t;read\n")
       (printf"\tstrb\tw0, [x20, x21]\n")
-      (emit-c instr-rst)]
+      (emit-asm instr-rst)]
      [(loop body)
       (let ([start-label (fresh-label)]
             ;; [body-label (fresh-label)]
@@ -101,10 +157,10 @@ _ptr:
         (printf "\tldrsb\tw11, [x20, x21]\n")      ; copy tape[ptr] to w11
         (printf "\tsubs\tw11, w11, #0\n")        ; w11 - 0; set the bit
         (printf "\tbeq\t~a\n" end-label)         ; exit loop
-        (emit-c body)
+        (emit-asm body)
         (printf "\tb\t~a\n" start-label)
         (printf "~a:\n" end-label))
-      (emit-c instr-rst)])])
+      (emit-asm instr-rst)])])
 
 (define (compile-file filename)
   (displayln "Parsing..." (current-error-port))
